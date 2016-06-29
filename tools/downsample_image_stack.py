7879bc2db55199701847d64854528999a9dff0b2
Brendan Shanny <brendanshanny@brendans-mbp.public.wireless.med.harvard.edu>
2016-06-29 10:44:17 -0400

    Added -i inres option, changed -r to -o for outres option, removed dimdir and doutdir

--- tools/downsample_image_stack.py
+++ tools/downsample_image_stack.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python
+import argparse
+import glob
 import imghdr
 import numpy
-import glob
 import os
-import argparse
+from PIL import Image
 import scipy.misc
 import scipy.ndimage
 import sys
-from PIL import Image
 
-dimdir = '/zflode/130201zf142/PSC/150820_60nmpxS1_linked/'
-doutdir = '/zflode/130201zf142/Russel/Test1200nmIsoGen_OrderChange/'
 
 inres = numpy.array([56.3, 56.3, 60.])
 outres = numpy.array([300., 300., 300.])
@@ -21,42 +19,17 @@ secsize = int(numpy.ceil(1 / scale[2]))
 default_slice_buff = int(numpy.ceil(1/scale[2]))
 
 
-def directory(path):
-    if not os.path.isdir(path):
-        err_msg = "path is not a directory (%s)"
-        raise argparse.ArgumentTypeError(err_msg)
-    return path
-
-# parse command line options
-parser = argparse.ArgumentParser()
-parser.add_argument(
-   '-s', '--source', type=directory, required=True,
-   help="Path to a source directory containing image files to be downsampled."
-        "[required]")
-parser.add_argument(
-   '-d', '--dest', type=directory, required=True,
-   help="Path to a destination directory. [required]")
-parser.add_argument(
-   '-r', '--sect', type=int, required=False,
-   help="Desired resolution of output image. [not required]")
-opts = parser.parse_args()
-
-source_path = opts.source
-dest_path = opts.dest
-res = opts.sect
-
-
-def getimageinfo(imdir):
+def getimageinfo(source_path):
     print 'Retrieving image info.  This may take some time.'
     imageinfo = {}
     global shape
     shape = None
-    for fil in os.listdir(imdir):
-        path = imdir + fil
+    for fil in os.listdir(source_path):
+        path = source_path + fil
         if imghdr.what(path):
             root, ext = os.path.splitext(fil)
             slc = slicefromroot(root)
-            imageinfo[slc] = {'path': imdir,
+            imageinfo[slc] = {'path': source_path,
                               'image': fil,
                               'extension': ext,
                               'fullpath': path}
@@ -89,10 +62,10 @@ def buildarray(images, minim, maxim, max_slice_buff=default_slice_buff):
 
 
 def save_images(img, start, zscale=scale[2]):
-    if not os.path.exists(outdir):
-        os.makedirs(outdir)
+    if not os.path.exists(dest_path):
+        os.makedirs(dest_path)
     for zcoord in range(len(img[0, 0])):
-        fn = outdir + '{}T_down.PGM'.format(str(int(start + (float(zcoord) *
+        fn = dest_path + '{}T_down.PGM'.format(str(int(start + (float(zcoord) *
                                                 1. / zscale))).zfill(5))
         if numpy.std(img[:, :, zcoord]) < 10:
             print numpy.std(img[:, :, zcoord])
@@ -119,15 +92,19 @@ def intrazpix(stack, secperpix):
                                                  axis=2)):
         print "averaging array of shape {}".format(subarr.shape)
         substack = numpy.ma.masked_invalid(subarr)
+        print "Substack Created!"
         subzs = numpy.nan_to_num(substack.data)
+        print 'Subzs Created!'
         newstack = scipy.ndimage.zoom(subzs,
                                       [scale[0], scale[1], 1.],
                                       order=1)
+        print "New Stack Created!"
         del subzs
         marray = numpy.ma.array(newstack,
                                 mask=numpy.resize(substack.mask,
                                                   newstack.shape))
         meanarr = numpy.ma.mean(marray, axis=2)
+        print "Mean Array Created!"
         try:
             newarr[:, :, j] = meanarr
         except:
@@ -151,19 +128,45 @@ def process_stack(images, slice_buff=default_slice_buff, onlyintra_zpix=True):
         if onlyintra_zpix:
             stack = intrazpix(stack, secsize)
         save_images(stack, i, zscale=scale[2])
+        print "Downsampled Imgaes Saved!"
+        print "Moving onto next set..."
+
+
+def directory(path):
+    if not os.path.isdir(path):
+        err_msg = "path is not a directory (%s)"
+        raise argparse.ArgumentTypeError(err_msg)
+    return path
+
+# parse command line options
+parser = argparse.ArgumentParser()
+parser.add_argument(
+   '-s', '--source', type=directory, required=True,
+   help="Path to a source directory containing image files to be downsampled."
+        "[required]")
+parser.add_argument(
+   '-d', '--dest', type=directory, required=True,
+   help="Path to a destination directory. [required]")
+parser.add_argument(
+   '-o', '--outres', type=int, required=False,
+   help="Desired resolution of output image. [not required]")
+parser.add_argument(
+   '-i', '--inres', type=int, required=False,
+   help="Desired resolution of input image. [not required]")
+opts = parser.parse_args()
+
+source_path = opts.source
+dest_path = opts.dest
+out_res = opts.outres
+in_res = opts.inres
+
 
 if __name__ == "__main__":
-    if source_path:
-        imdir = source_path
-    else:
-        imdir = dimdir
-    if dest_path:
-        outdir = dest_path
-    else:
-        outdir = doutdir
-    if res:
-        outres = numpy.array([res, res, res])
-    print outres
-    img_files = glob.glob(imdir)
+    if out_res:
+        outres = numpy.array([out_res, out_res, out_res])
+    if in_res:
+        inres = numpy.array([in_res, in_res, in_res])
+    print "Output Resolution: {}".format(outres)
+    img_files = glob.glob(source_path)
     imgs = getimageinfo(img_files[0])
     process_stack(imgs)
